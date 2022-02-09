780cb975f83fdc443cfa332a6c07a360491d49d8
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-09 23:18:28 +0530

    first commit

---
+++ src/test/java/com/jpa/learning/LearningJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
