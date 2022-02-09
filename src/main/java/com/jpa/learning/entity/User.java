54b65ae7ab06331bbf459b5f2f0a4bcf4b249217
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-10 00:17:34 +0530

    Dao service created

--- src/main/java/com/jpa/learning/entity/User.java
+++ src/main/java/com/jpa/learning/entity/User.java
@@ -1,6 +1,13 @@
 package com.jpa.learning.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
 	private long id;
 	private String name;
 	private String role;
