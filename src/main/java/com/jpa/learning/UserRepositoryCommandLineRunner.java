0af26efbac41fe5df7afff112b7b6810ef32a677
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-02-14 00:01:54 +0530

    Project finished

--- src/main/java/com/jpa/learning/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/jpa/learning/UserRepositoryCommandLineRunner.java
@@ -1,6 +1,9 @@
 package com.jpa.learning;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,11 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 		User user=new User("Jill","Admin");
 		userRepository.save(user);
 		log.info("New user is created: "+user);
+		
+		Optional<User> findById = userRepository.findById(1L);	
+		log.info("New user is retrieved: "+findById);
+		
+		List<User> findAll = userRepository.findAll();
+		log.info("All users: "+findAll);
 	}
-	
 }
